c407b6d581d4fe48f155c68e63d0b7709dfe8976
Unknown <up201605339@fe.up.pt>
2018-02-15 19:47:53 +0000

    end of class no. 1

--- cal_fp01/src/Factorial.cpp
+++ cal_fp01/src/Factorial.cpp
@@ -3,14 +3,34 @@
  */
 
 #include "Factorial.h"
+#include <vector>
 
-int factorialRecurs(int n)
-{
-	return 1;
+using namespace std;
+//espaço em tempo - n
+// espaço de memória - n, O(n)
+int factorialRecurs(int n) {
+
+	if (n == 1 || n == 0)
+		return 1;
+
+	return n * factorialRecurs(n - 1);
 
 }
+//espaço em tempo - n
+//espaço de memória - constante, é O(1);
+int factorialDinam(int n) {
 
-int factorialDinam(int n)
-{
-	return 1;
+	if (n == 1 || n == 0)
+		return 1;
+
+	int fact_value = 1;
+
+	for (int i = 2; i <= n; i++) {
+
+		fact_value *= i;
+
+	}
+
+	return fact_value;
+
 }
